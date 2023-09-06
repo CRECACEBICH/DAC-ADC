a644a30c670e6842e0da889caf1e330a68902fbc
CRECACEBICH <141262181+CRECACEBICH@users.noreply.github.com>
2023-09-06 15:15:35 -0300

    Create dac.h

---
+++ dac.h
@@ -0,0 +1,17 @@
+#ifndef dac_h
+#define dac_h
+
+#define enable_clock()
+#define enable_conv()
+#define enable_vref()
+#define enable_digitalvalue()
+
+
+void enable_clock();
+void enable_clock2();
+void enable_vref1();
+void enable_vref2();
+void enable_digitalvalue1();
+void enable_digitalvalue2();
+
+#endif
